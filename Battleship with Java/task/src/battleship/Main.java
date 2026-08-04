695d3400a716eb85143845a22e9f9a8d654118c8
Hassieb Hassieb <hassieb.n.h@gmail.com>
2026-08-03 21:18:36 -0700

    Tested new user input feature with error handling where user can place ships into the board

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -3,11 +3,12 @@ package battleship;
 
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userInput = "";
+        String userInput;
         String[][] board = {
                 {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},
                 {"A", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
@@ -21,6 +22,16 @@ public class Main {
                 {"I", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"},
                 {"J", "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"}
         };
+
+        String[] placingShips = {
+                "Enter the coordinates of the Aircraft Carrier (5 cells):",
+                "Enter the coordinates of the Battleship (4 cells):",
+                "Enter the coordinates of the Submarine (3 cells):",
+                "Enter the coordinates of the Cruiser (3 cells):",
+                "Enter the coordinates of the Destroyer (2 cells):"
+        };
+        boolean[][] adjacentChecker = new boolean[12][12];
+
         String[] coords =  new  String[5];
         char rowLetterOne = ' ', rowLetterTwo = ' ';
 
@@ -29,16 +40,42 @@ public class Main {
 
         int rowNumberTwo = 0;
         int colmTwoNumber = 0;
+        int count = 5;
+
+        //boardDisplay(board);
+
+//        board[1][1] = "O";
+//        board[1][2] = "O";
+//        board[1][3] = "O";
+//        board[1][4] = "O";
+//
+//        board[6][3] = "O";
+//        board[6][4] = "O";
+//        board[6][5] = "O";
+//        board[6][6] = "O";
+//        board[6][7] = "O";
+//
+//        board[7][10] = "O";
+//        board[8][10] = "O";
+//        board[9][10] = "O";
+//        board[10][10] = "O";
+
+
+
 
         boardDisplay(board);
 
-        System.out.println("Enter the coordinates of the ship:");
-        while (true) {
-            userInput = scanner.nextLine();
+
+
+        for (String msg : placingShips) {
+            System.out.println(msg + "\n");
+
+            while (true) {
+                userInput = scanner.nextLine();
 
             if (!userInput.matches("[A-J]([1-9]|10)\s[A-J]([1-9]|10)")) {
-                System.out.println("Error: invalid coordinates");
-                break;
+                System.out.println("\nError! Wrong ship location! Try again:\n");
+                continue;
             }
 
             coords =  userInput.split(" ");
@@ -51,15 +88,139 @@ public class Main {
 
             colmOneNumber = Integer.parseInt(coords[0].substring(1));
             colmTwoNumber = Integer.parseInt(coords[1].substring(1));
+            
+            int length = getLengthOfShips(coords);
 
             if (rowLetterOne != rowLetterTwo && colmOneNumber != colmTwoNumber) {
-                System.out.println("Error: invalid coordinates");
-                break;
+                System.out.println("\nError! Wrong ship location! Try again:\n");
+                continue;
             }
 
-            break;
+            if (board[rowNumberOne][colmOneNumber].equals("O")) {
+                System.out.println("\nError! Wrong ship location! Try again:\n");
+                continue;
+            }
+
+            if (board[rowNumberTwo][colmTwoNumber].equals("O")) {
+                System.out.println("\nError! You placed it too close to another one. Try again:\n");
+                continue;
+            }
+
+            if (adjacentChecker[rowNumberOne][colmOneNumber]) {
+                System.out.println("\nError! You placed it too close to another one. Try again:\n");
+                continue;
+            }
+
+            if (msg.contains("5") && length != 5) {
+                System.out.println("\nError! Wrong length of the Aircraft! Try again:\n");
+                continue;
+            }
+            else if (msg.contains("4") && length != 4) {
+                System.out.println("\nError! Wrong length of the Battleship! Try again:\n");
+                continue;
+            }
+            else if (msg.contains("Submarine") && msg.contains("3") && length != 3) {
+                System.out.println("\nError! Wrong length of the Submarine! Try again:\n");
+                continue;
+            }
+            else if (msg.contains("Cruiser") && msg.contains("3") && length != 3) {
+                System.out.println("\nError! Wrong length of the Cruiser! Try again:\n");
+                continue;
+            }
+            else if (msg.contains("2") && length != 2) {
+                System.out.println("\nError! Wrong length of the Destroyer! Try again:\n");
+                continue;
+            }
+
+            // A1 A4
+            if (rowLetterOne == rowLetterTwo && colmOneNumber < colmTwoNumber) {
+                for (int i = colmOneNumber; i <= colmTwoNumber; i++) {
+                    board[rowNumberOne][i] = "O";
+
+                    adjacentChecker[rowNumberOne][i - 1] = true;
+                    adjacentChecker[rowNumberOne + 1][i - 1] = true;
+                    adjacentChecker[rowNumberOne - 1][i - 1] = true;
+                }
+            }
+            // A4 A1
+            else if (rowLetterOne == rowLetterTwo && colmOneNumber > colmTwoNumber) {
+                for (int i = colmOneNumber; i >= colmTwoNumber ; i--) {
+                    board[rowNumberOne][i] = "O";
+
+                    adjacentChecker[rowNumberOne][i - 1] = true;
+                    adjacentChecker[rowNumberOne + 1][i - 1] = true;
+                    adjacentChecker[rowNumberOne - 1][i - 1] = true;
+                }
+            }
+            // A1 D1
+            else if (rowLetterOne != rowLetterTwo && rowNumberOne < rowNumberTwo) {
+                for (int i = rowNumberOne; i <= rowNumberTwo; i++) {
+                    board[i][colmOneNumber] = "O";
+
+                    adjacentChecker[i - 1][colmOneNumber] = true;
+                    adjacentChecker[i - 1][colmOneNumber + 1] = true;
+                    adjacentChecker[i - 1][colmOneNumber - 1] = true;
+                }
+            }
+            // D1 D3
+            else if (rowLetterOne != rowLetterTwo && rowNumberOne > rowNumberTwo) {
+                for (int i = rowNumberOne; i >= rowNumberTwo; i--) {
+                    board[i][colmOneNumber] = "O";
+
+                    adjacentChecker[i - 1][colmOneNumber] = true;
+                    adjacentChecker[i - 1][colmOneNumber + 1] = true;
+                    adjacentChecker[i - 1][colmOneNumber - 1] = true;
+                }
+            }
+
+
+
+                boardDisplay(board);
+
+                break;
+            }
         }
 
+
+        /*
+                F3 F4 F5 F6 F7
+
+
+                (6,3) (6,4) (6,5) (6,6) (6,7)
+
+         */
+
+
+
+
+
+
+    }
+
+    public static void boardDisplay(String[][] board) {
+        System.out.println();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            if (i < board.length - 1) {
+                System.out.println();
+            }
+        }
+        System.out.println("\n");
+    }
+
+
+    public static int getLengthOfShips (String[] coords) {
+
+        char rowLetterOne = coords[0].charAt(0);
+        char rowLetterTwo = coords[1].charAt(0);
+
+        int rowNumberOne = (rowLetterOne - 'A') + 1;
+        int rowNumberTwo = (rowLetterTwo - 'A') + 1;
+
+        int colmOneNumber = Integer.parseInt(coords[0].substring(1));
+        int colmTwoNumber = Integer.parseInt(coords[1].substring(1));
         int length = 0;
 
         if (rowLetterOne == rowLetterTwo && colmOneNumber == 1) {
@@ -80,41 +241,8 @@ public class Main {
         else if (rowLetterOne != rowLetterTwo && rowNumberOne > rowNumberTwo) {
             length = (rowNumberOne - rowNumberTwo) + 1;
         }
-
-        System.out.println("Length: " + length);
-        System.out.print("Parts: ");
-        if (rowLetterOne == rowLetterTwo && colmOneNumber < colmTwoNumber) {
-            for (int i = colmOneNumber; i <= colmTwoNumber; i++) {
-                System.out.printf("%c%d ", rowLetterOne, i);
-            }
-        }
-        else if (rowLetterOne == rowLetterTwo && colmOneNumber > colmTwoNumber) {
-            for (int i = colmOneNumber; i >= colmTwoNumber ; i--) {
-                System.out.printf("%c%d ", rowLetterOne, i);
-            }
-        }
-        else if (rowLetterOne != rowLetterTwo && rowNumberOne < rowNumberTwo) {
-            for (int i = rowNumberOne; i <= rowNumberTwo; i++) {
-                System.out.print(board[i][0] + colmOneNumber + " ");
-            }
-        }
-        else if (rowLetterOne != rowLetterTwo && rowNumberOne > rowNumberTwo) {
-            for (int i = rowNumberOne; i >= rowNumberTwo; i--) {
-                System.out.print(board[i][0] + colmOneNumber + " ");
-            }
-        }
-
+        return length;
     }
 
-    public static void boardDisplay(String[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            if (i < board.length - 1) {
-                System.out.println();
-            }
-        }
-        System.out.println();
-    }
+
 }
